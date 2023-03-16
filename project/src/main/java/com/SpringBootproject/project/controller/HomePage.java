de3701ffde9d6258a95849b5d034e919bc8a85f4
Srivenkateswara <venki.sankiti@gmail.com>
2023-03-15 22:21:41 -0500

    spring Intialsetup

--- project/src/main/java/com/SpringBootproject/project/controller/HomePage.java
+++ project/src/main/java/com/SpringBootproject/project/controller/HomePage.java
@@ -1,2 +1,27 @@
-package com.SpringBootproject.project.controller;public class HomePage {
+package com.SpringBootproject.project.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+@RequestMapping(value = "/homepage")
+public class HomePage {
+
+    private final  static  Logger log = LoggerFactory.getLogger(HomePage.class);
+
+
+    @GetMapping(value = "/helloWorld")
+    public String index() {
+
+        log.warn("from the index page  please see the end point");
+        return "helloWorld";
+    }
+
+
 }
